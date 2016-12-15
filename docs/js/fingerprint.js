3ec76459a33379260a5fec012d1325d49fa8d75c
Long Ouyang <longouyang@gmail.com>
2016-12-14 18:50:03 -0800

    More robust fingerprinting

--- docs/js/fingerprint.js
+++ docs/js/fingerprint.js
@@ -1,6 +1,24 @@
 var fingerprint;
 
+// conservatively, just get the IP (should always work, as long as web.stanford.edu doesn't go down)
+function setIp(ip) {
+  console.log('set ip');
+  fingerprint.ip = ip;
+
+  // now, try to get more detailed geolocation info (will work if freegeoip is up and we haven't hit their limit)
+  var isLocal = /file/.test(location.protocol);
+  var protocol = isLocal ? "http://" : "//";
+
+  var scriptEl = document.createElement('script');
+  var src = protocol + "web.stanford.edu/~louyang/cgi-bin/locate.php?callback=setGeo";
+  scriptEl.src = src;
+
+  document.body.appendChild(scriptEl);
+}
+
+// try to get geo-located data
 function setGeo(data) {
+  console.log('set geo');
   fingerprint.ip = data.ip;
   fingerprint.geo = data;
 }
@@ -29,11 +47,11 @@ function setGeo(data) {
   var isLocal = /file/.test(location.protocol);
 
   // inject a call to a json service that will give us geolocation information
-  var protocol = isLocal ? "http://" : "//";
-  var src = protocol + "freegeoip.net/json/?callback=setGeo";
-
   var scriptEl = document.createElement('script');
-  scriptEl.src = protocol + "freegeoip.net/json/?callback=setGeo";
+  var protocol = isLocal ? "http://" : "//";
+  var src = protocol + "web.stanford.edu/~louyang/cgi-bin/locate2.php?callback=setIp";
+  scriptEl.src = src;
+
 
   document.body.appendChild(scriptEl);
 
